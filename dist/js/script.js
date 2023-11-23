3036c0f690708dbf7619cdf35b958974a58b69ff
ivan matheu <ivan.matheu@edu.univ-fcomte.fr>
2023-11-23 18:21:40 +0700

    v2, darkmode in progress

--- dist/js/script.js
+++ dist/js/script.js
@@ -12,6 +12,18 @@ document.addEventListener("DOMContentLoaded", function() {
         else {
             header.classList.remove('navbar-fixed');
         }
+
+        const body = document.body;
+        const toTop = document.querySelector('#to-top');
+        if(window.scrollY < 3/4 * body.scrollHeight) {
+            toTop.classList.add('hidden');
+            toTop.classList.remove('flex');
+        }
+        else {
+            toTop.classList.remove('hidden');
+            toTop.classList.add('flex');
+        }
+       
     }
 
 
@@ -23,4 +35,31 @@ document.addEventListener("DOMContentLoaded", function() {
         hambuger.classList.toggle('hamburger-active');
         navMenu.classList.toggle('hidden');
     });
+
+
+    // Clicked outside of hamburger menu area
+
+    window.addEventListener('click', (e) => {
+        if(e.target !== hambuger && e.target !== navMenu) {
+            hambuger.classList.remove('hamburger-active');
+            navMenu.classList.add('hidden');
+        }
+    });
+
+
+
+    // Dark Toggle
+
+    const darkToggle = document.querySelector('#dark-toggle');
+    const html = document.querySelector('html');
+    let a = 1;
+
+    darkToggle.addEventListener('click' , () => {
+        if(darkToggle.checked) {
+            darkToggle.classList.add('nanana')
+        }
+        else {
+            darkToggle.classList.remove('nanana');
+        }
+    });    
 });
